ab93e99705f277a46a22368db3c5659a00026e0a
Cody Elzey <g.i.coding@Codys-MacBook-Pro.local>
2023-06-07 10:20:07 -0500

    we've come together

--- lambda.tf
+++ lambda.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-provider "aws" {
-  region = "us-east-1"
-}
-
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
