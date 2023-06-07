1b5541f3539e0fe1d140f7a2f36381f7112742f2
Cody Elzey <g.i.coding@Codys-MacBook-Pro.local>
2023-06-07 15:02:28 -0500

    maybe it'll work

--- amplify.tf
+++ amplify.tf
@@ -1,27 +1,27 @@
 resource "aws_amplify_app" "cypress-project3" {
-    name       = var.amplify-name
-    repository = var.amplify-repo
-    access_token = var.github_token
+  name         = var.amplify-name
+  repository   = var.amplify-repo
+  access_token = "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"
 
-    build_spec = file("./buildspec.yaml")
-    enable_auto_branch_creation = true
+  build_spec                  = file("./buildspec.yaml")
+  enable_auto_branch_creation = true
 
   # The default patterns added by the Amplify Console.
-    auto_branch_creation_patterns = [
+  auto_branch_creation_patterns = [
     "*",
     "*/**",
   ]
 
-    auto_branch_creation_config {
+  auto_branch_creation_config {
     # Enable auto build for the created branch.
     enable_auto_build = true
 
-    }
+  }
 
-    custom_rule {
-      source = "/<*>"
-      status = "404"
-      target = "/index.html"
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
   }
 
 }
@@ -30,7 +30,7 @@ resource "aws_amplify_branch" "main" {
   app_id      = aws_amplify_app.cypress-project3.id
   branch_name = "main"
 
-  stage     = "PRODUCTION"
+  stage = "PRODUCTION"
 }
 
 output "Run_build" {
