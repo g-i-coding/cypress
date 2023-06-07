b8318d9ba584969d72b755778baa4ee9ed7ed86f
MajinBeans <dean.baeten@gmail.com>
2023-06-07 14:02:38 -0400

    Condensed all files into one folder except src

---
+++ vars.tf
@@ -0,0 +1,45 @@
+#API Gateway Variables#
+variable "endpoint_path" {
+  description = "The GET endpoint path"
+  type = string
+  default = "method"
+}
+
+variable "myregion" {
+    description = "AWS region"
+    type = string
+    default = "us-east-1"
+}
+
+variable "accountId" {
+    description = "AWS account ID"
+    type = string
+}
+
+variable "lambda_function_name" {
+    description = "What to name the lambda function"
+    type = string
+    default = "cypress-lambda"
+}
+##VPC Variables
+variable "amplify-name" {
+  type = string
+  default = "cypress-project3"
+  description = "The name of the app"
+
+}
+
+
+variable "amplify-repo" {
+  type = string
+  default = "https://github.com/Rizi0/amplify"
+  description = "Repo amplify pulls from"
+}
+
+
+
+variable "github_token" {
+  type    = string
+  default = null
+  sensitive = true
+}
