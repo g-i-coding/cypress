7f25032958ace68ba84a66c8f3375150140ebfa7
MajinBeans <dean.baeten@gmail.com>
2023-06-08 15:51:27 -0400

    changes

--- api_gateway.tf
+++ api_gateway.tf
@@ -59,6 +59,13 @@ resource "aws_api_gateway_stage" "api-stage" {
     stage_name = "dev"
 }
 
+module "api-gateway-enable-cors" {
+  source  = "squidfunk/api-gateway-enable-cors/aws"
+  version = "0.3.3"
+  rest_api_id = aws_api_gateway_rest_api.TeamCypressAPI.id
+  resource_id = aws_api_gateway_resource.TeamCypressAPIresource.id
+}
+
 output "api_url" {
     value = "${aws_api_gateway_deployment.api-deployment.invoke_url}"
 }
