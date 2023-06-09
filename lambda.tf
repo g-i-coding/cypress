dbe227837e84c80505c7bec600046ff47c1c56a6
EC2 Default User <ec2-user@ip-172-31-66-112.ec2.internal>
2023-06-09 21:42:06 +0000

    api still doesn't work,lambda/db does

--- lambda.tf
+++ lambda.tf
@@ -44,24 +44,7 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
 
-# data "aws_iam_policy_document" "assume_role" {
-#   statement {
-#     effect = "Allow"
 
-#     principals {
-#       type        = "Service"
-#       identifiers = ["lambda.amazonaws.com"]
-#     }
-
-#     actions = ["sts:AssumeRole"]
-#   }
-# }
-
-
-# resource "aws_iam_role" "iam_for_lambda" {
-#   name               = "iam_for_lambda"
-#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-# }
 
 data "archive_file" "lambda" {
   type        = "zip"
