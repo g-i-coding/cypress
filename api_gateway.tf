c5a13b6dc7ba5d8e1b4dc4e22890163b0bbeddf1
EC2 Default User <ec2-user@ip-172-31-66-112.ec2.internal>
2023-06-10 19:02:11 +0000

    hopefully working api

--- api_gateway.tf
+++ api_gateway.tf
@@ -9,20 +9,93 @@ resource "aws_api_gateway_resource" "TeamCypressAPIresource" {
     path_part = var.endpoint_path
 }
 
-resource "aws_api_gateway_method" "TeamCypressAPI_Get-Method" {
+resource "aws_api_gateway_method" "TeamCypressAPI_Get_Method" {
     rest_api_id = aws_api_gateway_rest_api.TeamCypressAPI.id
     resource_id = aws_api_gateway_resource.TeamCypressAPIresource.id
     http_method = "GET"
     authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_method" "TeamCypressAPI_Post_Method" {
     rest_api_id = aws_api_gateway_rest_api.TeamCypressAPI.id
     resource_id = aws_api_gateway_resource.TeamCypressAPIresource.id
-    http_method = aws_api_gateway_method.TeamCypressAPI_Get-Method.http_method
+    http_method = "POST"
+    authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "Get_integration" {
+    rest_api_id = aws_api_gateway_rest_api.TeamCypressAPI.id
+    resource_id = aws_api_gateway_resource.TeamCypressAPIresource.id
+    http_method = aws_api_gateway_method.TeamCypressAPI_Get_Method.http_method
     integration_http_method = "POST"
     type = "AWS_PROXY"
     uri = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "Post_integration" {
+    rest_api_id = aws_api_gateway_rest_api.TeamCypressAPI.id
+    resource_id = aws_api_gateway_resource.TeamCypressAPIresource.id
+    http_method = aws_api_gateway_method.TeamCypressAPI_Post_Method.http_method
+    integration_http_method = "POST"
+    type = "AWS_PROXY"
+    uri = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_method" "TeamCypress_options" {
+  rest_api_id   = aws_api_gateway_rest_api.TeamCypressAPI.id
+  resource_id   = aws_api_gateway_resource.TeamCypressAPIresource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "TeamCypress_options_200" {
+  rest_api_id = aws_api_gateway_rest_api.TeamCypressAPI.id
+  resource_id = aws_api_gateway_resource.TeamCypressAPIresource.id
+  http_method = aws_api_gateway_method.TeamCypress_options.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "TeamCypress_options" {
+  rest_api_id = aws_api_gateway_rest_api.TeamCypressAPI.id
+  resource_id = aws_api_gateway_resource.TeamCypressAPIresource.id
+  http_method = aws_api_gateway_method.TeamCypress_options.http_method
+
+  type = "MOCK"
+  
+  request_templates = {
+    "application/json" = <<EOF
+{
+  "statusCode" : 200
+}
+EOF
+  }
+}
+
+resource "aws_api_gateway_integration_response" "TeamCypress_options" {
+  rest_api_id = aws_api_gateway_rest_api.TeamCypressAPI.id
+  resource_id = aws_api_gateway_resource.TeamCypressAPIresource.id
+  http_method = aws_api_gateway_method.TeamCypress_options.http_method
+  status_code = aws_api_gateway_method_response.TeamCypress_options_200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
 }
 
 resource "aws_lambda_permission" "teamcypressapigw_lambda" {
@@ -30,28 +103,34 @@ resource "aws_lambda_permission" "teamcypressapigw_lambda" {
     action = "lambda:InvokeFunction"
     function_name = aws_lambda_function.lambda.function_name
     principal = "apigateway.amazonaws.com"
-    source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.TeamCypressAPI.id}/*/${aws_api_gateway_method.TeamCypressAPI_Get-Method.http_method}${aws_api_gateway_resource.TeamCypressAPIresource.path}"
+    source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.TeamCypressAPI.id}/*/${aws_api_gateway_method.TeamCypressAPI_Get_Method.http_method}${aws_api_gateway_resource.TeamCypressAPIresource.path}"
 }
 
-resource "aws_api_gateway_deployment" "api-deployment" {
+resource "aws_api_gateway_deployment" "api_deployment" {
+    depends_on = [
+        aws_api_gateway_integration.Get_integration, 
+        aws_api_gateway_integration.Post_integration, 
+        aws_api_gateway_integration.TeamCypress_options 
+        
+    ]
     rest_api_id = aws_api_gateway_rest_api.TeamCypressAPI.id
 
-    triggers = {
-        redeployment = sha1(jsonencode(aws_api_gateway_rest_api.TeamCypressAPI.body))
-    }
+    # triggers = {
+    #     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.TeamCypressAPI.body))
+    # }
 
-    lifecycle {
-        create_before_destroy = true
-    }
-    depends_on = [aws_api_gateway_method.TeamCypressAPI_Get-Method, aws_api_gateway_integration.integration]
+    # lifecycle {
+    #     create_before_destroy = true
+    # }
+    
 }
 
 resource "aws_api_gateway_stage" "api-stage" {
-    deployment_id = aws_api_gateway_deployment.api-deployment.id
+    deployment_id = aws_api_gateway_deployment.api_deployment.id
     rest_api_id = aws_api_gateway_rest_api.TeamCypressAPI.id
     stage_name = "dev"
 }
 
 output "api_url" {
-    value = "${aws_api_gateway_deployment.api-deployment.invoke_url}"
+    value = "${aws_api_gateway_deployment.api_deployment.invoke_url}"
 }
